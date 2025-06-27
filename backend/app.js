3acb7882d6798a8896af9f87e247316b64929a85
smooth-dev <azhar.yassine2015@gmail.com>
2025-06-27 14:43:22 +0100

    implemented fetch imgs from backend file

--- backend/app.js
+++ backend/app.js
@@ -50,4 +50,6 @@ app.use((req, res, next) => {
   res.status(404).json({ message: '404 - Not Found' });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(`✅ Server running on http://localhost:3000`);
+});
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
+const places = localStorage.getItem('places');
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces,setAvailablePlaces] =  useState([]);
+
+useEffect(()=>
+  {
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places');
+      const  resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchPlaces();
+
+  }
+
+,[]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
