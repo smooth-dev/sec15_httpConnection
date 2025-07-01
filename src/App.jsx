9cbcfdfaa40590b3793dbf0314ebec7043181217
smooth-dev <azhar.yassine2015@gmail.com>
2025-07-01 23:53:41 +0100

    added error, loading  handling and put method to the backend writing into a file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import { updateUserPlaces } from './http.js';
 
 function App() {
   const selectedPlace = useRef();
@@ -22,7 +23,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function  handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,7 +33,12 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
-  }
+try
+{   await  updateUserPlaces([selectedPlace,...userPlaces]);
+} catch(error){
+ console.log(error)
+
+} }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
     setUserPlaces((prevPickedPlaces) =>
@@ -42,6 +48,7 @@ function App() {
     setModalIsOpen(false);
   }, []);
 
+  
   return (
     <>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,16 +1,45 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 const places = localStorage.getItem('places');
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching,setIsFetching] = useState(false);
   const [availablePlaces,setAvailablePlaces] =  useState([]);
+  const [error,setError] = useState();
 
 useEffect(()=>
   {
     async function fetchPlaces(){
-      const response = await fetch('http://localhost:3000/places');
-      const  resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      setIsFetching(true);
+
+      try{
+      const places = await fetchAvailablePlaces();
+   
+         navigator.geolocation.getCurrentPosition((position)=>{
+
+          const sortedPlaces =  sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          )
+         setAvailablePlaces(sortedPlaces);
+
+         })
+
+
+         setIsFetching(false);
+
+
+        }
+      catch (error){
+        setError({message: error.message || "Could not fetch places, please try again later"});
+        setIsFetching(false);
+
+      }
     }
 
     fetchPlaces();
@@ -19,11 +48,17 @@ useEffect(()=>
 
 ,[]);
 
+if(error){
+  return <ErrorPage title="An error occured" message={error.message}/>
+}
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
+      isLoading={isFetching}
+      loadingText={"Fetching places !"}
       onSelectPlace={onSelectPlace}
     />
   );
