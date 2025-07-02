015b59d886bef8044d2bdefc6967cb71f668187a
smooth-dev <azhar.yassine2015@gmail.com>
2025-07-03 00:50:27 +0100

    completed

--- src/http.js
+++ src/http.js
@@ -29,3 +29,15 @@ const response = await fetch('http://localhost:3000/user-places',{
     return resData.message;
 
 }
+
+
+export async function fetchUserPlaces( ){
+const response = await fetch('http://localhost:3000/user-places');
+      const resData = await response.json();
+
+      if(!response.ok) {
+         throw new Error('Failed to fetch user places')}
+
+
+         return resData.places;
+}
